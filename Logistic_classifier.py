9a873ca866898e8cd959d94f914e3eeab9d1592f
Hamza CHETTOUR <hamzachettour@gmail.com>
2018-07-31 00:48:06 +0200

    feat: Add plotting and plot accuracy graph

--- Logistic_classifier.py
+++ Logistic_classifier.py
@@ -2,7 +2,8 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf 
 import numpy as np 
-from Batching import get_batches 
+from Batching import get_batches
+import matplotlib.pyplot as plt
 
 size_inputs=784 # images are 28*28
 size_classes=10 # 10 digits ( 0-9 ) 
@@ -53,19 +54,28 @@ right_prediction=tf.equal(tf.argmax(logits,1),tf.argmax(labels,1))
 #Calculating accuracy : which is the proportion of true predicitons 
 accuracy=tf.reduce_mean(tf.cast(right_prediction, tf.float32))
 
-#Define a function to print stats at the end of each epoch : loss and accuracy 
-def print_stats(epoch_i,ses, current_features, current_labels):
-	current_loss=ses.run(loss,feed_dict={features:current_features, labels:current_labels})
-	validation_accuracy=ses.run(accuracy,feed_dict={features: V_features, labels: V_labels})
-	print(' Epoch = {}  | Loss = {:3.4} | Validation Accuracy = {:3.4}'.format(epoch_i,current_loss,validation_accuracy))
-# init for variables 
-init=tf.global_variables_initializer()
-
-
 # Tuning happens here : 
 batch_size=50
 epochs=300
 learn_rate=0.01
+
+#Create variables to plot 
+Epoch_plot=np.arange(epochs)
+Accuracy_plot=[]
+Loss_plot=[]
+
+#Define a function to print stats at the end of each epoch : loss and accuracy 
+def print_stats(epoch_i,ses, current_features, current_labels):
+	current_loss=ses.run(loss,feed_dict={features:current_features, labels:current_labels})
+	Loss_plot.append(current_loss)
+	validation_accuracy=ses.run(accuracy,feed_dict={features: V_features, labels: V_labels})
+	print(' Epoch = {}  | Loss = {:3.4} | Validation Accuracy = {:3.4}'.format(epoch_i,current_loss,validation_accuracy))
+	Accuracy_plot.append(validation_accuracy)
+# init for variables 
+init=tf.global_variables_initializer()
+
+
+
 
 train_batches=get_batches(batch_size, T_features, T_labels)
 
@@ -85,7 +95,13 @@ with tf.Session() as ses:
 	test_accuracy=ses.run(accuracy,feed_dict={features: TS_features, labels: TS_labels})
 print('Test Accuracy :{}'.format(test_accuracy))
 
-
+#plot the evolution of accuracy and cost 
+plt.plot(Epoch_plot,Accuracy_plot,'b',label="Accuracy")
+plt.plot(Epoch_plot,Loss_plot,'r',label="Loss")
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+plt.savefig('./Results/graphs/accuracy_cost.png')
+plt.show()
 
 
 
